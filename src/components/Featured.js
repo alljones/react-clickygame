d032c6d5d92d4646a22b0b64987e38bfa741133f
Allan Jones <iamallanjones@gmail.com>
2018-06-21 23:19:54 -0500

    Added Styles to Component elements

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,12 +1,12 @@
 // Jumbotron 
 
 import React, { Component } from 'react';
-//import './App.css';
+import './Featured.css';
 
 class Featured extends Component {
   render() {
     return (
-        <div className="jumbotron text-center">
+        <div className="jumbotron featured">
             <h1 className="display-4">Clicky Game!</h1>
             <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
         </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,15 @@
 // Footer
 
 import React, { Component } from 'react';
-//import './App.css';
+import './Footer.css';
 
 class Footer extends Component {
   render() {
     return (
-        <footer className="container">
+        <footer className= "footer">
+          <div className="container">
             <p>Copyright © 2018 Allan Jones. All rights reserved.</p>
+          </div>
         </footer>
 
           
